7545a73fbb4cbd29808b3d824bd20099f76ff567
bbeda <bogdan.beda@force24.co.uk>
2024-12-28 18:56:41 +0200

    - updated function host setup to include user secrets config provider - added local.setting.json to git as it is needed for local development

--- BuxferImporter.Workers.Functions/Program.cs
+++ BuxferImporter.Workers.Functions/Program.cs
@@ -1,20 +1,30 @@
 using BuxferImporter.Core;
 using BuxferImporter.Revolut;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var builder = FunctionsApplication.CreateBuilder(args);
-
-builder.Services.AddMemoryCache();
-builder.Services.RegisterRevolutServices();
-builder.Services.RegisterCoreServices(builder.Configuration);
-
-builder.ConfigureFunctionsWebApplication();
+var hostBuilder = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureAppConfiguration((context, builder) =>
+    {
+        if (context.HostingEnvironment.IsDevelopment())
+        {
+            _ = builder.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true);
+            _ = builder.AddUserSecrets<Program>();
+        }
+    })
+    .ConfigureServices((context, services) =>
+    {
+        _ = services.AddMemoryCache();
+        _ = services.RegisterRevolutServices();
+        _ = services.RegisterCoreServices(context.Configuration);
+    });
 
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
 // builder.Services
 //     .AddApplicationInsightsTelemetryWorkerService()
 //     .ConfigureFunctionsApplicationInsights();
 
-builder.Build().Run();
+hostBuilder.Build().Run();

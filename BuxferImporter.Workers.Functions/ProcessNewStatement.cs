98c28bdd788ce7aa3ebd7b15e880bd454d82e912
bbeda <bogdan.beda@force24.co.uk>
2024-12-28 19:29:56 +0200

    - updated file matching to accept wildcards - added support to output processing information to output blob

--- BuxferImporter.Workers.Functions/ProcessNewStatement.cs
+++ BuxferImporter.Workers.Functions/ProcessNewStatement.cs
@@ -4,7 +4,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System.ComponentModel.Design;
+using System.IO.Enumeration;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BuxferImporter.Workers.Functions;
@@ -26,21 +26,37 @@ public class ProcessNewStatement
     }
 
     [Function(nameof(ProcessNewStatement))]
-    public async Task Run([BlobTrigger("statements", Connection = "AzureWebJobsStorage")] BlobClient blobClient, FunctionContext functionContext)
+    public async Task Run([BlobTrigger("statements", Connection = "AzureWebJobsStorage")] BlobClient inputBlobClient, FunctionContext functionContext)
     {
-        var matchingMapping = _mappingOptions.Value.Entries.FirstOrDefault(x => x.File == blobClient.Name);
+        var matchingMapping = _mappingOptions.Value.Entries.FirstOrDefault(x => FileSystemName.MatchesSimpleExpression(x.File, inputBlobClient.Name, true));
+
+        var blobServiceClient = new BlobServiceClient("UseDevelopmentStorage=true");
+        var containerClient = blobServiceClient.GetBlobContainerClient("output");
+        _ = await containerClient.CreateIfNotExistsAsync();
+
+        var fileName = $"{inputBlobClient.Name}-{DateTime.UtcNow:yyyyMMddHHmmss}.txt";
+        var blobClientOutput = containerClient.GetBlobClient(fileName);
+
+        using var outputStream = await blobClientOutput.OpenWriteAsync(true);
+        using var writer = new StreamWriter(outputStream, leaveOpen: true);
+
+        await writer.WriteLineAsync($"Processing {inputBlobClient.Name}");
 
         if (matchingMapping == null)
         {
-            _logger.LogWarning($"No mapping found for {blobClient.Name}");
+            _logger.LogWarning($"No mapping found for {inputBlobClient.Name}");
+            await writer.WriteLineAsync($"No mapping found");
             return;
         }
+        await writer.WriteLineAsync(matchingMapping.ToString());
+        await writer.FlushAsync();
 
         var processor = serviceProvider.GetRequiredKeyedService<StatementMappingProcessor>(matchingMapping.StatementType);
 
-        using var stream = await blobClient.OpenReadAsync();
+        using var stream = await inputBlobClient.OpenReadAsync();
         var skippedCount = 0;
         var totalCount = 0;
+
         await foreach (var result in processor.ImportAsync(matchingMapping.Account, stream))
         {
             totalCount++;
@@ -50,10 +66,11 @@ public class ProcessNewStatement
                 continue;
             }
             _logger.LogInformation($"{result.ResultType} {result.StatementId} {result.BuxferId} {result.Details}");
-
-
+            await writer.WriteLineAsync(result.ToString());
+            await writer.FlushAsync();
         }
         var footer = $"Total:{totalCount}, Skipped:{skippedCount}";
         _logger.LogInformation(footer);
+        await writer.WriteLineAsync(footer);
     }
 }
